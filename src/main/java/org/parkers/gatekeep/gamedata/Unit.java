4e8ea968699fdf43c2fe33f2ee0d887eb597cf25
Rakned <my.spot@verizon.net>
2020-05-02 11:12:11 -0400

    add unit snowflake ID code

--- src/main/java/org/parkers/gatekeep/gamedata/Unit.java
+++ src/main/java/org/parkers/gatekeep/gamedata/Unit.java
@@ -1,8 +1,11 @@
 package org.parkers.gatekeep.gamedata;
 
+import discord4j.core.object.util.Snowflake;
+
 class Unit {
     MyImage portrait;
     int x, y;
+    private Snowflake user;
 
     Unit(MyImage image, int tsize) {
         x = -1;
@@ -11,11 +14,19 @@ class Unit {
         portrait = image.copyResize(tsize);
     }
 
+    Unit(MyImage image, int tsize, Snowflake owner) {
+        this(image, tsize);
+        user = owner;
+    }
+
     void setPos(int x, int y) {
         this.x = x;
         this.y = y;
     }
     boolean isAt(int x, int y) {
         return (this.x == x) && (this.y == y);
+    }
+    boolean isUser(Snowflake s) {
+        return user == null || user.equals(s);
     }
 }
